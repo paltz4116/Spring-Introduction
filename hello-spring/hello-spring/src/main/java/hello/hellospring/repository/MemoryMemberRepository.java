daf5477302bdea1ba563d24d8421503c088a3e0b
pfalz <mega75581@gmail.com>
2024-01-07 16:49:42 +0900

    added memberServie and test case

--- hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,7 @@ import hello.hellospring.domain.Member;
 import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
-    private static Map<Long, Member> store = new HashMap<>();
+    private static final Map<Long, Member> store = new HashMap<>();
     private static long sequence = 0L;
     @Override
     public Member save(Member member) {
